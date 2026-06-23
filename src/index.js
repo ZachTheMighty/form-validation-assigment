d4abb2d22b82be016b407bd78b30d9a3dfe3af99
ZachTheMighty <promoted66@gmail.com>
2026-06-23 19:09:52 +0000

    add: alert box for when all inputs are valid upon form submission

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import validatePassword from "./validators/password.js";
 import validateConfrim from "./validators/confirm.js";
 
 const form = document.querySelector("form");
+const inputs = document.querySelectorAll("label + *");
+
 const email = document.querySelector("#email");
 const country = document.querySelector("#country");
 const postal = document.querySelector("#postal");
@@ -13,11 +15,18 @@ const password = document.querySelector("#password");
 const confirm = document.querySelector("#confirm");
 
 form.addEventListener("submit", (event) => {
+  let isAllValid = true;
   event.preventDefault();
   validateEmail(email);
   validatePostal(country, postal);
   validatePassword(password);
   validateConfrim(password, confirm);
+
+  inputs.forEach((input) => {
+    if (!input.checkValidity()) isAllValid = false;
+  });
+
+  if (isAllValid) alert("high five");
 });
 
 email.addEventListener("input", () => validateEmail(email));
